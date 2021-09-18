c6b3d43d8a36cfbd2b613a74f765c606d4c86929
bsuth <bsuth701@gmail.com>
2021-09-18 23:56:48 +0900

    remove old reference code

--- src/init.lua
+++ src/init.lua
@@ -2,74 +2,26 @@ local inspect = require('inspect')
 local lpeg = require('lpeg')
 
 local env = require('env')
-local oldrules = require('oldrules')
 local rules = require('rules')
 local supertable = require('supertable')
 
--- -----------------------------------------------------------------------------
--- Erde
--- -----------------------------------------------------------------------------
-
 lpeg.setmaxstack(1000)
-local oldgrammar = lpeg.P(oldrules.parser)
+
 local parsergrammar = lpeg.P(rules.parser)
 local compilergrammar = lpeg.P(rules.compiler)
 local formattergrammar = lpeg.P(rules.formatter)
-local erde = {}
 
--- -----------------------------------------------------------------------------
--- Parser
--- -----------------------------------------------------------------------------
-
-function erde.parse(subject)
-  env:reset()
-  return parsergrammar:match(subject, nil, {}) or {}
-end
-
-function erde.compile(subject)
-  env:reset()
-  return compilergrammar:match(subject, nil, {})
-end
-
-function erde.format(subject)
-  env:reset()
-  return formattergrammar:match(subject, nil, {})
-end
-
--- -----------------------------------------------------------------------------
--- Compiler
--- -----------------------------------------------------------------------------
-
-local function isnode(node)
-  return type(node) == 'table' and type(node.rule) == 'string'
-end
-
-function erde.compilenode(node)
-  if not isnode(node) then
-    return subnode
-  elseif type(oldrules.compiler[node.rule]) == 'function' then
-    return oldrules.compiler[node.rule](unpack(node:ipairs():reduce(function(args, subnode)
-      return args:push(unpack(isnode(subnode)
-        and { erde.compilenode(subnode) }
-        or { subnode }
-      ))
-    end, supertable())))
-  end
-end
-
-function erde.oldcompile(subject)
-  env:reset()
-  local ast = oldgrammar:match(subject, nil, {}) or {}
-  return erde.compilenode(ast)
-end
-
-function erde.compile(subject)
-  env:reset()
-  return compilergrammar:match(subject, nil, {})
-end
-
--- -----------------------------------------------------------------------------
--- Return
--- -----------------------------------------------------------------------------
-
-return erde
+return {
+  parse = function(subject)
+    env:reset()
+    return parsergrammar:match(subject, nil, {}) or {}
+  end,
+  compile = function(subject)
+    env:reset()
+    return compilergrammar:match(subject, nil, {})
+  end,
+  format = function(subject)
+    env:reset()
+    return formattergrammar:match(subject, nil, {})
+  end,
+}
