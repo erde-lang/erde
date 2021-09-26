8fe62932867c5c8111fc6151486a3b52fc795680
bsuth <bsuth701@gmail.com>
2021-09-27 07:49:55 +0900

    add erde eval, e2e if/else + for spec

--- erde/init.lua
+++ erde/init.lua
@@ -4,23 +4,34 @@ local supertable = require('erde.supertable')
 local inspect = require('inspect')
 local lpeg = require('lpeg')
 
+local erde = {}
 lpeg.setmaxstack(1000)
 
 local parsergrammar = lpeg.P(rules.parser)
 local compilergrammar = lpeg.P(rules.compiler)
 local formattergrammar = lpeg.P(rules.formatter)
 
-return {
-  parse = function(subject)
-    state:reset()
-    return parsergrammar:match(subject, nil, {}) or {}
-  end,
-  compile = function(subject)
-    state:reset()
-    return compilergrammar:match(subject, nil, {})
-  end,
-  format = function(subject)
-    state:reset()
-    return formattergrammar:match(subject, nil, {})
-  end,
-}
+function erde.parse(subject)
+  state:reset()
+  return parsergrammar:match(subject, nil, {}) or {}
+end
+
+function erde.compile(subject)
+  state:reset()
+  return compilergrammar:match(subject, nil, {})
+end
+
+function erde.format(subject)
+  state:reset()
+  return formattergrammar:match(subject, nil, {})
+end
+
+function erde.eval(erdecode)
+  if _VERSION:find('5.1') then
+    return loadstring(erde.compile(erdecode))()
+  else
+    return load(erde.compile(erdecode))()
+  end
+end
+
+return erde
--- spec/compiler/Logic_spec.lua
+++ spec/compiler/Logic_spec.lua
@@ -1,11 +1,72 @@
 local erde = require('erde')
 
-local function run(erdecode)
-  return (loadstring or load)(erde.compile(erdecode))()
-end
-
 spec('if else', function()
-  assert.are.equal(1, run('if true { return 1 }'))
-  assert.are.equal('if true then  elseif true then  end', erde.compile('if true {} elseif true {}'))
-  assert.are.equal('if true then  else  end', erde.compile('if true {} else {}'))
+  assert.are.equal(1, erde.eval([[
+    if true {
+      return 1
+    }
+  ]]))
+  assert.are.equal(2, erde.eval([[
+    if false {
+      return 1
+    } elseif true {
+      return 2
+    }
+  ]]))
+  assert.are.equal(3, erde.eval([[
+    if false {
+      return 1
+    } elseif false {
+      return 2
+    } else {
+      return 3
+    }
+  ]]))
+  assert.are.equal(4, erde.eval([[
+    if false {
+      return 1
+    } elseif false {
+      return 2
+    } elseif false {
+      return 3
+    } else {
+      return 4
+    }
+  ]]))
+end)
+
+spec('numeric for', function()
+  assert.are.equal(15, erde.eval([[
+    local x = 0
+    for i = 1, 5 {
+      x = x + i
+    }
+    return x
+  ]]))
+  assert.are.equal(15, erde.eval([[
+    local x = 0
+    for i = 5, 1, -1 {
+      x = x + i
+    }
+    return x
+  ]]))
+end)
+
+spec('generic for', function()
+  assert.are.equal(30, erde.eval([[
+    local x = 0
+    local t = { 5, 10, 15 }
+    for key, value in ipairs(t) {
+      x = x + value
+    }
+    return x
+  ]]))
+  assert.are.equal(6, erde.eval([[
+    local x = 0
+    local t = { 5, 10, 15 }
+    for key, value in ipairs(t) {
+      x = x + key
+    }
+    return 6
+  ]]))
 end)
