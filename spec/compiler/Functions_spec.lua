f923da9a8018b27f4bf68cec84e8a46d2122ffc1
brian.sutherland <brian.sutherland@mujin.co.jp>
2021-09-27 12:25:34 +0900

    finish function declaration tests

--- spec/compiler/Functions_spec.lua
+++ spec/compiler/Functions_spec.lua
@@ -2,15 +2,31 @@ local erde = require('erde')
 
 spec('function declaration', function()
   assert.are.equal(1, erde.eval([[
-    local function test() {
+    function test() {
       return 1
     }
     return test()
   ]]))
   assert.are.equal(5, erde.eval([[
-    local function test(a, b) {
+    function test(a, b) {
       return a + b
     }
     return test(2, 3)
+  ]]))
+  assert.are.equal(1, erde.eval([[
+    function test(a) { return a }
+    do {
+      local function test(a) {
+        return -a
+      }
+    }
+    return test(1)
+  ]]))
+  assert.are.equal(-1, erde.eval([[
+    function test(a) { return a }
+    local function test(a) {
+      return -a
+    }
+    return test(1)
   ]]))
 end)
