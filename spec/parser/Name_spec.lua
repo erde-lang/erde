7a6e35716a3f7af8915543c0832c538ef6610de7
brian.sutherland <brian.sutherland@mujin.co.jp>
2021-10-20 17:51:44 +0900

    allow underscore in names, prevent keyword names, add Name_spec

---
+++ spec/parser/Name_spec.lua
@@ -0,0 +1,39 @@
+local unit = require('erde.parser.unit')
+
+local KEYWORDS = {
+  'local',
+  'global',
+  'if',
+  'elseif',
+  'else',
+  'for',
+  'in',
+  'while',
+  'repeat',
+  'until',
+  'do',
+  'function',
+  'false',
+  'true',
+  'nil',
+  'return',
+}
+
+spec('valid names', function()
+  assert.are.equal('x', unit.Name('x').value)
+  assert.are.equal('hello', unit.Name('hello').value)
+  assert.are.equal('hello_world', unit.Name('hello_world').value)
+  assert.are.equal('h1', unit.Name('h1').value)
+end)
+
+spec('invalid names', function()
+  assert.has_error(function()
+    unit.Name('1h')
+  end)
+
+  for _, keyword in pairs(KEYWORDS) do
+    assert.has_error(function()
+      unit.Name(keyword)
+    end)
+  end
+end)
