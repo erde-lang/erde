0576b8302f0c79ddf734e4ed73b4517be7feea30
bsuth <bsuth701@gmail.com>
2021-10-27 22:06:21 +0900

    cleanup forloop parser

--- spec/parser/ForLoop_spec.lua
+++ spec/parser/ForLoop_spec.lua
@@ -1,8 +1,19 @@
 local unit = require('erde.parser.unit')
 
-spec('valid numeric for', function()
+spec('for loop rule', function()
+  assert.are.equal('ForLoop', unit.ForLoop('for i = 1, 2 {}').rule)
+end)
+
+spec('numeric for loop', function()
   assert.has_subtable({
-    rule = 'ForLoop',
+    variant = 'numeric',
+    name = 'i',
+    var = { value = '1' },
+    limit = { value = '2' },
+  }, unit.ForLoop(
+    'for i = 1, 2 {}'
+  ))
+  assert.has_subtable({
     variant = 'numeric',
     name = 'i',
     var = { value = '1' },
@@ -11,35 +22,16 @@ spec('valid numeric for', function()
   }, unit.ForLoop(
     'for i = 1, 2, 3 {}'
   ))
-  assert.has_subtable({
-    rule = 'ForLoop',
-    variant = 'numeric',
-    name = 'i',
-    var = { value = '1' },
-    limit = { value = '2' },
-  }, unit.ForLoop(
-    'for i = 1, 2 {}'
-  ))
-end)
-
-spec('invalid numeric for', function()
   assert.has_error(function()
     unit.ForLoop('for i = 1 {}')
   end)
   assert.has_error(function()
-    unit.ForLoop('for i = 1, 2, {}')
-  end)
-  assert.has_error(function()
     unit.ForLoop('for i = 1, 2, 3, 4 {}')
-  end)
-  assert.has_error(function()
-    unit.ForLoop('for i = 1, 2')
   end)
 end)
 
-spec('valid generic for', function()
+spec('generic for loop', function()
   assert.has_subtable({
-    rule = 'ForLoop',
     variant = 'generic',
     nameList = { 'a' },
     exprList = { { value = '1' } },
@@ -47,7 +39,6 @@ spec('valid generic for', function()
     'for a in 1 {}'
   ))
   assert.has_subtable({
-    rule = 'ForLoop',
     variant = 'generic',
     nameList = { 'a', 'b' },
     exprList = { { value = '1' } },
@@ -55,7 +46,6 @@ spec('valid generic for', function()
     'for a, b in 1 {}'
   ))
   assert.has_subtable({
-    rule = 'ForLoop',
     variant = 'generic',
     nameList = { 'a' },
     exprList = {
@@ -66,7 +56,6 @@ spec('valid generic for', function()
     'for a in 1, 2 {}'
   ))
   assert.has_subtable({
-    rule = 'ForLoop',
     variant = 'generic',
     nameList = { 'a', 'b' },
     exprList = {
@@ -76,19 +65,4 @@ spec('valid generic for', function()
   }, unit.ForLoop(
     'for a, b in 1, 2 {}'
   ))
-end)
-
-spec('invalid generic for', function()
-  assert.has_error(function()
-    unit.ForLoop('for a 1 {}')
-  end)
-  assert.has_error(function()
-    unit.ForLoop('for a in {}')
-  end)
-  assert.has_error(function()
-    unit.ForLoop('for in 1 {}')
-  end)
-  assert.has_error(function()
-    unit.ForLoop('for a in 1')
-  end)
 end)
