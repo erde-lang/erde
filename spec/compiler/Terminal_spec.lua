7e03c49ce7134a4799b68f15134313ecf3a02821
bsuth <bsuth701@gmail.com>
2021-11-06 16:48:38 +0900

    IMPORTANT: remove old compiler / parser infra code

--- spec/compiler/Terminal_spec.lua
+++
@@ -1,8 +0,0 @@
-local constants = require('erde.constants')
-local unit = require('erde.compiler.unit')
-
-spec('terminals', function()
-  for _, terminal in pairs(constants.TERMINALS) do
-    assert.are.equal(terminal, unit.Terminal(terminal))
-  end
-end)
