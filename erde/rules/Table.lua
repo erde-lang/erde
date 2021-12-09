9d8e347f771ef94b8082a81fa317c430c3bbeb2e
bsuth <bsuth701@gmail.com>
2021-12-10 07:44:10 +0900

    remove inline table keys

--- erde/rules/Table.lua
+++ erde/rules/Table.lua
@@ -8,18 +8,6 @@ local Table = { ruleName = 'Table' }
 -- Parse
 -- -----------------------------------------------------------------------------
 
-local function parseInlineKeyField(ctx)
-  -- TODO: better syntax?
-  if not ctx:branchChar(':') then
-    ctx:throwExpected(':')
-  end
-
-  return {
-    variant = 'inlineKey',
-    key = ctx:Name().value,
-  }
-end
-
 local function parseExprKeyField(ctx)
   local field = {
     variant = 'exprKey',
@@ -63,7 +51,6 @@ function Table.parse(ctx)
       allowTrailingComma = true,
       rule = function()
         return ctx:Switch({
-          parseInlineKeyField,
           parseExprKeyField,
           parseNameKeyField,
           parseNumberKeyField,
@@ -96,15 +83,13 @@ function Table.compile(ctx, node)
       else
         local spreadField = {}
 
-        if field.variant == 'inlineKey' or field.variant == 'nameKey' then
+        if field.variant == 'nameKey' then
           spreadField.key = '"' .. field.key .. '"'
         elseif field.variant ~= 'numberKey' then
           spreadField.key = ctx:compile(field.key)
         end
 
-        spreadField.value = field.variant == 'inlineKey' and field.key
-          or ctx:compile(field.value)
-
+        spreadField.value = ctx:compile(field.value)
         spreadFields[i] = spreadField
       end
     end
@@ -116,9 +101,7 @@ function Table.compile(ctx, node)
     for i, field in ipairs(node) do
       local fieldPart
 
-      if field.variant == 'inlineKey' then
-        fieldPart = field.key .. ' = ' .. field.key
-      elseif field.variant == 'nameKey' then
+      if field.variant == 'nameKey' then
         fieldPart = field.key .. ' = ' .. ctx:compile(field.value)
       elseif field.variant == 'numberKey' then
         fieldPart = ctx:compile(field.value)
