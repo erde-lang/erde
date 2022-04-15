bc521c6e4bb9aa93531ddd905080ee61b4264ebf
bsuth <bsuth701@gmail.com>
2022-04-15 21:04:59 +0900

    update erde loader to not mutate global require

--- erde/loader.lua
+++ erde/loader.lua
@@ -1,35 +1,31 @@
--- Loader for `*.erde` files. Mutates the global require function.
 local erde = require('erde')
-local nativeRequire = require
--- TODO: figure out why global `package` not available in local require
-local package = _G.package
 local pathSep = package.config:sub(1, 1)
 
-function require(moduleName)
-  moduleName = moduleName:gsub('%.', pathSep)
+-- TODO: use package.erdepath? similar to moonscript
 
-  if package.loaded[moduleName] ~= nil then
-    return package.loaded[moduleName]
-  end
+local function erdeLoader(moduleName)
+  moduleName = moduleName:gsub('%.', pathSep)
 
   for path in package.path:gmatch('[^;]+') do
     local erdePath = path:gsub('%.lua', '.erde'):gsub('?', moduleName)
-    moduleFile = io.open(erdePath)
+    local moduleFile = io.open(erdePath)
 
     if moduleFile ~= nil then
-      local module = erde.run(moduleFile:read('*a'))
-
-      -- Keep consistent behavior w/
-      -- https://www.lua.org/manual/5.1/manual.html#pdf-require
-      if module ~= nil then
-        package.loaded[moduleName] = module
-      else
-        package.loaded[moduleName] = true
+      return function()
+        return erde.run(moduleFile:read('*a'))
       end
-
-      return module
     end
   end
 
-  return nativeRequire(moduleName)
+  return 'Failed to find module'
 end
+
+local loaders = package.loaders or package.searchers
+
+for loader in ipairs(loaders) do
+  if loader == erdeLoader then
+    return
+  end
+end
+
+table.insert(loaders, erdeLoader)
