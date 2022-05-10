59ecf2d76245e01fbc21ae23f8f566f5a0598002
brian.sutherland <brian.sutherland@mujin.co.jp>
2022-05-11 08:45:02 +0900

    more tests / organizing

--- spec/format/Statement_spec.lua
+++
@@ -1,9 +0,0 @@
-spec('break / continue', function()
-  assert.formatted(' break ', 'break')
-  assert.formatted(' continue ', 'continue')
-end)
-
-spec('goto', function()
-  assert.formatted(' goto   blah ', 'goto blah')
-  assert.formatted('::   blah ::', ' ::blah:: ')
-end)
