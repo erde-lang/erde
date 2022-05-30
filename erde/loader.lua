bcdbc3774fa9dfcdc9e2ac8cee76919e196e104b
brian.sutherland <brian.sutherland@mujin.co.jp>
2022-05-31 08:55:07 +0900

    refactor loader

--- erde/loader.lua
+++ erde/loader.lua
@@ -1,28 +1,40 @@
 local erde = require('erde')
 local pathSep = package.config:sub(1, 1)
 
-local function erdeLoader(moduleName)
-  moduleName = moduleName:gsub('%.', pathSep)
+local function erdeSearcher(moduleName)
+  modulePath = moduleName:gsub('%.', pathSep)
 
   for path in package.path:gmatch('[^;]+') do
-    local erdePath = path:gsub('%.lua', '.erde'):gsub('?', moduleName)
-    local moduleFile = io.open(erdePath)
+    local fullModulePath = path:gsub('%.lua', '.erde'):gsub('?', modulePath)
+    local moduleFile = io.open(fullModulePath)
 
     if moduleFile ~= nil then
+      moduleFile:close()
+
       return function()
-        return erde.run(moduleFile:read('*a'))
+        local moduleFile = io.open(fullModulePath)
+
+        if moduleFile == nil then
+          return 'File no longer exists: ' .. fullModulePath
+        end
+
+        local moduleContents = moduleFile:read('*a')
+        moduleFile:close()
+        return erde.run(moduleContents)
       end
     end
   end
 end
 
-local loaders = package.loaders or package.searchers
+-- https://www.lua.org/manual/5.1/manual.html#pdf-package.loaders
+-- https://www.lua.org/manual/5.3/manual.html#pdf-package.searchers
+local searchers = package.loaders or package.searchers
 
-for loader in ipairs(loaders) do
-  if loader == erdeLoader then
+for searcher in ipairs(searchers) do
+  if searcher == erdeSearcher then
     return
   end
 end
 
--- Place erde loader first to prioritize erde files over lua ones.
-table.insert(loaders, 1, erdeLoader)
+-- Place erde loader directly after preloader to prioritize erde files over lua.
+table.insert(searchers, 2, erdeSearcher)
