9ead528d2bf71e0436238ac24a3f6df2441a4bd0
brian.sutherland <brian.sutherland@mujin.co.jp>
2022-09-30 10:31:49 +0900

    fix repl bugs

--- erde/repl.lua
+++ erde/repl.lua
@@ -1,80 +1,66 @@
 local RL = require('readline')
 local C = require('erde.constants')
 local lib = require('erde.lib')
+local utils = require('erde.utils')
 
 local PROMPT = '> '
 local SUB_PROMPT = '>> '
 
--- TODO support strings
-local SURROUND_OPENING_CHARS = { ['('] = ')', ['{'] = '}', ['['] = ']' }
-local SURROUND_CLOSING_CHARS = { [')'] = '(', ['}'] = '{', [']'] = '[' }
-
-RL.set_readline_name('erde')
 RL.set_options({
   keeplines = 1000,
   histfile = '~/.erde_history',
   completion = false,
   auto_add = false,
 })
+RL.set_readline_name('erde')
 
-local function updateSurroundCounters(line, surroundCounters)
-  for i = 1, #line do
-    local char = line:sub(i, i)
-    if SURROUND_OPENING_CHARS[char] ~= nil then
-      surroundCounters[char] = surroundCounters[char] + 1
-    elseif SURROUND_CLOSING_CHARS[char] ~= nil then
-      local openingChar = SURROUND_CLOSING_CHARS[char]
-      surroundCounters[openingChar] = math.max(0, surroundCounters[openingChar] - 1)
-    end
+local function readLine(prompt)
+  local replLine = RL.readline(prompt)
+  if not replLine or replLine:match('^%s*$') then return end
+
+  local sourceLines = {}
+  local needsSubPrompt = false
+
+  -- Check all lines from readline in case user is reusing a previous multiline command
+  for _, line in pairs(utils.split(replLine, '\n')) do
+    table.insert(sourceLines, (line:gsub('\\%s*$', '')))
   end
 
-  for char, counter in pairs(surroundCounters) do
-    if counter > 0 then
-      return true
-    end
-  end
-
-  return false
-end
-
-local function getLine()
-  local line = RL.readline(PROMPT)
-
-  if line and #line > 0 then
-    local surroundCounters = { ['('] = 0, ['{'] = 0, ['['] = 0 }
-    local needsSubPrompt = updateSurroundCounters(line, surroundCounters)
-
-    if needsSubPrompt then
-      repeat
-        local subLine = RL.readline(SUB_PROMPT)
-        needsSubPrompt = updateSurroundCounters(subLine, surroundCounters)
-        line = line .. '\n' .. subLine
-      until not needsSubPrompt or not subLine
-    end
-
-    RL.add_history(line)
-    return line
-  end
+  return replLine, table.concat(sourceLines, '\n'), replLine:match('\\%s*$')
 end
 
 return function()
   print('erde ' .. C.VERSION .. '  Copyright (C) 2021-2022 bsuth')
-  local line = getLine()
+  print('Use trailing backslashes for multiline inputs.')
 
-  while line do
+  while true do
+    local replLine, sourceLine, needsSubPrompt = readLine(PROMPT)
+    if not replLine then break end
+
+    if needsSubPrompt then
+      repeat
+        local subReplLine, subSourceLine, needsSubPrompt = readLine(SUB_PROMPT)
+        if not subReplLine then break end
+        replLine = replLine .. '\n' .. subReplLine
+        sourceLine = sourceLine .. '\n' .. subSourceLine
+      until not needsSubPrompt
+    end
+
+    RL.add_history(replLine)
+
     -- Try expressions first! This way we can still print the value in the
     -- case that the expression is also a valid block (i.e. function calls).
-    local exprOk, exprResult = pcall(function() return lib.run('return ' .. line, 'stdin') end)
-    
+    local exprOk, exprResult = pcall(function() return lib.run('return ' .. sourceLine, 'stdin') end)
+
     if exprOk then
       if exprResult ~= nil then
         print(exprResult)
       end
     elseif exprResult.type ~= 'compile' then
-      print(exprResult.stacktrace)
+      print(exprResult.stacktrace or exprResult.message)
     else
-      local blockOk, blockResult = pcall(function() return lib.run(line, 'stdin') end)
-      
+      local blockOk, blockResult = pcall(function() return lib.run(sourceLine, 'stdin') end)
+
       if blockOk then
         if blockResult ~= nil then
           print(blockResult)
@@ -87,7 +73,5 @@ return function()
         print('block: ' .. tostring(blockResult))
       end
     end
-
-    line = getLine()
   end
 end
